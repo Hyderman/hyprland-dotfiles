5c234a92c6005c5b9b99439dc7ce6c5a8906f9f4
Hyderman <logan.faurisson@gmail.com>
2023-06-18 22:42:30 +0200

    update waybar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,9 @@
-if status is-login
-	if test (tty) = /dev/tty1
-		# wrappedhl
-	end
-end
-
 if status is-interactive
 	# Commands to run in interactive sessions can go here
     set -U fish_greeting
 	starship init fish | source
 	alias r="ranger"
+    bind \ce "ranger"
 end
 
 fish_add_path /home/hyderman/.local/bin
--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 swayidle -w \
     timeout 300 "swaylock && hyprctl dispatcher dpms off" \
-    timeout 600 "systemctl suspend"
+    timeout 600 "systemctl suspend" \
+    after-resume sudo modprobe -r hid_logitech_dj && sudo modprobe hid_logitech_dj 
+    
