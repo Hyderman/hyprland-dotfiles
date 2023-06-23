0adf3b5d51b2df7d7173ffbaa887c2120c25df5b
Hyderman <logan.faurisson@gmail.com>
2023-06-23 19:06:55 +0200

    correct suspend script

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -2,5 +2,5 @@
 swayidle -w \
     timeout 300 "swaylock && hyprctl dispatcher dpms off" \
     timeout 600 "systemctl suspend" \
-    after-resume sudo modprobe -r hid_logitech_dj && sudo modprobe hid_logitech_dj 
+    after-resume "sudo modprobe -r hid_logitech_dj && sudo modprobe hid_logitech_dj"
     
