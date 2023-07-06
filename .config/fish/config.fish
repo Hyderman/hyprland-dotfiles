4f861c6b22744ad33d7b451e06011b98557d958b
Hyderman <logan.faurisson@gmail.com>
2023-07-06 06:27:22 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,4 @@ if status is-interactive
 	# Commands to run in interactive sessions can go here
     set -U fish_greeting
 	starship init fish | source
-    set fish_vi_force_cursor 1
-    set fish_cursor_default block
-    set fish_cursor_insert line
-    set fish_cursor_replace_one underscore
-    set fish_cursor_visual block
 end
